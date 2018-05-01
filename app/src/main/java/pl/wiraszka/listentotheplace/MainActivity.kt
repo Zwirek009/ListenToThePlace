a51b3839afaaccdd574e75f0551d2d783fcfb7be
Zwirek009 <maciek.wiraszka@gmail.com>
2018-05-01 21:59:51 +0300

    NA: add full fragment switch support

--- app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
+++ app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
@@ -4,25 +4,32 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import pl.wiraszka.listentotheplace.fragments.AddMusicFragment
+import pl.wiraszka.listentotheplace.fragments.MusicListFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val addMusicFragment: AddMusicFragment
+    private val musicListFragment: MusicListFragment
+
+    init {
+        addMusicFragment = AddMusicFragment()
+        musicListFragment = MusicListFragment()
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        when(item.itemId){
+            R.id.navigation_add_music -> transaction.replace(R.id.fragment_container, addMusicFragment)
+            R.id.navigation_music_list -> transaction.replace(R.id.fragment_container, musicListFragment)
         }
-        false
+
+        transaction.commit()
+
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,5 +37,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, musicListFragment)
+        transaction.commit()
     }
 }
