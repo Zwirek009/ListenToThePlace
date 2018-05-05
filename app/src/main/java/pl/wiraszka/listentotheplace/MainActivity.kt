f5bcb8e23c03363aab92bee2c144f6fcf3a6f562
Zwirek009 <maciek.wiraszka@gmail.com>
2018-05-06 00:03:11 +0300

    Location: add basic location configuration

--- app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
+++ app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
@@ -1,13 +1,32 @@
 package pl.wiraszka.listentotheplace
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.multidex.MultiDex
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.wiraszka.listentotheplace.fragments.AddMusicFragment
 import pl.wiraszka.listentotheplace.fragments.MusicListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    private val TAG = "MainActivity"
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    private var mLocationManager: LocationManager? = null
+    lateinit var mLocation: Location
+    private var mLocationRequest: LocationRequest? = null
+    private val listener: com.google.android.gms.location.LocationListener? = null
+    lateinit var locationManager: LocationManager
 
     private val addMusicFragment: AddMusicFragment
     private val musicListFragment: MusicListFragment
@@ -36,10 +55,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MultiDex.install(this)
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+
+        mLocationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.fragment_container, musicListFragment)
         transaction.commit()
+    }
+
+    override fun onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    override fun onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+        Log.i(TAG, "Connection Suspended");
+        mGoogleApiClient.connect();
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        //
+        //    return;
+        //}
+    }
+
+    override fun onLocationChanged(location: Location) {
+
+
+        //var msg = "Updated Location: Latitude " + location.longitude.toString() + location.longitude;
+        //txt_latitude.setText(""+location.latitude);
+        //txt_longitude.setText(""+location.longitude);
+        //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+
     }
 }
