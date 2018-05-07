56ea2388742b4f633cd4c3c04375b730dee3c65b
Zwirek009 <maciek.wiraszka@gmail.com>
2018-05-07 23:23:08 +0300

    MobileApp: add basic location service

--- app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
+++ app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
@@ -1,20 +1,30 @@
 package pl.wiraszka.listentotheplace
 
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.support.design.widget.BottomNavigationView
 import android.support.multidex.MultiDex
 import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnSuccessListener
 import kotlinx.android.synthetic.main.activity_main.*
+import pl.wiraszka.listentotheplace.R.id.add_longtitude_input
+import pl.wiraszka.listentotheplace.R.styleable.View
 import pl.wiraszka.listentotheplace.fragments.AddMusicFragment
 import pl.wiraszka.listentotheplace.fragments.MusicListFragment
 
@@ -26,6 +36,8 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, G
     lateinit var mLocation: Location
     private var mLocationRequest: LocationRequest? = null
     private val listener: com.google.android.gms.location.LocationListener? = null
+    private val UPDATE_INTERVAL = (2 * 1000).toLong()  /* 10 secs */
+    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
     lateinit var locationManager: LocationManager
 
     private val addMusicFragment: AddMusicFragment
@@ -65,6 +77,8 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, G
 
         mLocationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+        checkLocation()
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val transaction = supportFragmentManager.beginTransaction()
@@ -96,22 +110,70 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, G
         Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
     }
 
-    override fun onConnected(p0: Bundle?) {
-
-        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        //
-        //    return;
-        //}
+    override fun onLocationChanged(location: Location) {
+        add_longtitude_input_temp.setText(""+location.longitude)
+        add_latitude_input_temp.setText(""+location.latitude)
     }
 
-    override fun onLocationChanged(location: Location) {
+    override fun onConnected(p0: Bundle?) {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
 
 
-        //var msg = "Updated Location: Latitude " + location.longitude.toString() + location.longitude;
-        //txt_latitude.setText(""+location.latitude);
-        //txt_longitude.setText(""+location.longitude);
-        //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        startLocationUpdates();
 
+        var fusedLocationProviderClient :
+                FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient .getLastLocation()
+                .addOnSuccessListener(this, OnSuccessListener<Location> { location ->
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        // Logic to handle location object
+                        mLocation = location;
+                        add_latitude_input_temp.setText("" + mLocation.latitude)
+                        add_longtitude_input_temp.setText("" + mLocation.longitude)
+                    }
+                })
+    }
 
+    private fun checkLocation(): Boolean {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showAlert() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " + "use this app")
+                .setPositiveButton("Location Settings", DialogInterface.OnClickListener { paramDialogInterface, paramInt ->
+                    val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(myIntent)
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { paramDialogInterface, paramInt -> })
+        dialog.show()
+    }
+
+    protected fun startLocationUpdates() {
+
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                mLocationRequest, this);
     }
 }
