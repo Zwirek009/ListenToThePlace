b3b92aab4cf95d5f757c30f9f3a85228f044f99d
Zwirek009 <maciek.wiraszka@gmail.com>
2018-05-07 22:12:26 +0300

    NA: work in progress

--- app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
+++ app/src/main/java/pl/wiraszka/listentotheplace/MainActivity.kt
@@ -1,5 +1,9 @@
 package pl.wiraszka.listentotheplace
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -11,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private val addMusicFragment: AddMusicFragment
     private val musicListFragment: MusicListFragment
+    private var locationManager : LocationManager
 
     init {
         addMusicFragment = AddMusicFragment()
@@ -41,5 +46,44 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.fragment_container, musicListFragment)
         transaction.commit()
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        checkLocation()
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener)
+
+
     }
+
+    private fun checkLocation(): Boolean {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showAlert() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " + "use this app")
+                .setPositiveButton("Location Settings", DialogInterface.OnClickListener { paramDialogInterface, paramInt ->
+                    val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(myIntent)
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { paramDialogInterface, paramInt -> })
+        dialog.show()
+    }
+        //define the listener
+        val locationListener: LocationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                thetext.setText("" + location.longitude + ":" + location.latitude);
+            }
+
+            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {}
+        }
 }
